9350431f9c073f87642e2c31d4a720a93287f131
Roman666shiyanov <60029536+Roman666shiyanov@users.noreply.github.com>
2020-12-12 02:55:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,114 +1,113 @@
-﻿using System;
+using System;
 using System.IO;
 namespace log
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //get n
-            
-            Console.WriteLine("Введите N");
-            var n = int.Parse(Console.ReadLine());
-            Log("Введите N : " + n.ToString() + '\n');
-            int max13, maxEven, beforeMax, max;
-            max13 = maxEven = beforeMax = max = default;
+class Program
+{
+static void Main(string[] args)
+{
+//ВВо n
 
-            //get values
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine($"Введите {i + 1}-ое число");
-                var current = int.Parse(Console.ReadLine());
-                Log("Введите N : " + current.ToString() + '\n');
-                if (current % 13 == 0 && current > max13)
-                {
-                    max13 = current;
-                }
-                if (current % 2 == 0 && current > maxEven)
-                {
-                    maxEven = current;
-                }
+Log("Начало" + '\n');
+var n = GetNumber("Введите N");
+Log("Введите N : " + n.ToString() + '\n');
 
-                if (current > max)
-                {
-                    beforeMax = max;
-                    max = current;
-                }
-                else if (current > beforeMax)
-                {
-                    beforeMax = current;
-                }
-            }
+int max13, maxEven, beforeMax, max;
+max13 = maxEven = beforeMax = max = default;
 
-            //get control value
-            Console.WriteLine("Введите контрольное значение");
-            var ctrl = int.Parse(Console.ReadLine());
-            Log("Введите контрольное значение : " + ctrl.ToString() + '\n');
+//get values
+for (int i = 0; i < n; i++)
+{
+var current = GetNumber($"Введите {i + 1}-ое число");
+Log($"Введите {i +1}-ое число: " + current.ToString() + '\n');
+if (current % 13 == 0 && current > max13)
+{
+max13 = current;
+}
+if (current % 2 == 0 && current > maxEven)
+{
+maxEven = current;
+}
 
-            //check control value
-            if (max13 == 0)
-            {
-                Console.WriteLine(false);
-                Log("" + false + '\n');
-                Console.WriteLine("Контроль не пройден");
+if (current > max)
+{
+beforeMax = max;
+max = current;
+}
+else if (current > beforeMax)
+{
+beforeMax = current;
+}
+}
 
+//get control value
+var ctrl = GetNumber("Введите контрольное значение");
+Log("Введите контрольное значение : " + ctrl.ToString() + '\n');
 
-            }
-            else
-            {
-                var res = 0;
-                if (max13 % 2 == 0)
-                {
-                    if (!max13.Equals(max))
-                    {
-                        res = max13 * max;
-                        Log("Значение 1 :" + res + '\n');
-                    }
-                    else
-                    {
-                        res = max13 * beforeMax;
-                        Log("Значение 2 :" + res + '\n');
-                    }
-                }
-                else
-                {
-                    res = max13 * maxEven;
-                    Log("Значение 3 :" + res + '\n');
-                }
+//check control value
+if (max13 == 0)
+{
+Console.WriteLine(false);
+Log("" + false + '\n');
+Console.WriteLine("Контроль не пройден");
 
 
-                Console.WriteLine(res);
-                Console.WriteLine(res.Equals(ctrl));
-                Console.WriteLine("Контроль пройден");
-                Log("" + true + '\n');
-                Log("" + "Контроль пройден" + '\n');
+}
+else
+{
+var res = 0;
+if (max13 % 2 == 0)
+{
+if (!max13.Equals(max))
+{
+res = max13 * max;
+Log("Значение 1 :" + res + '\n');
+}
+else
+{
+res = max13 * beforeMax;
+Log("Значение 2 :" + res + '\n');
+}
+}
+else
+{
+res = max13 * maxEven;
+Log("Значение 3 :" + res + '\n');
+}
 
-            }
-            static int GetNumber(string title)
-            {
-                while (true)
-                {
-                    try
-                    {
-                        Console.WriteLine(title);
-                        var textValue = Console.ReadLine();
-                        if (!int.TryParse(textValue, out int result))
-                        {
-                            throw new Exception($"Не удалось конвертировать {textValue} в число");
-                        }
 
-                        return result;
-                    }
-                    catch (Exception exception)
-                    {
-                        Console.WriteLine(exception.Message);
-                    }
-                }
-            }
-            static void Log(string message)
-            {
-                File.AppendAllText("C:\\44log.txt", message + Environment.NewLine);
-            }
-        }
-    }
+Console.WriteLine(res);
+Console.WriteLine(res.Equals(ctrl));
+Console.WriteLine("Контроль пройден");
+Log("" + true + '\n');
+Log("" + "Контроль пройден" + '\n');
+
+}
+static int GetNumber(string title)
+{
+while (true)
+{
+try
+{
+Console.WriteLine(title);
+var textValue = Console.ReadLine();
+if (!int.TryParse(textValue, out int result))
+{
+throw new Exception($"Не удалось конвертировать {textValue} в число");
+}
+
+return result;
+}
+catch (Exception exception)
+{
+Console.WriteLine(exception.Message);
+}
+}
+}
+static void Log(string message)
+{
+File.AppendAllText("C:\\44log.txt", message + Environment.NewLine);
+}
+}
+}
 }
